692b48a03cc5afc65b98e4bc1e708b4388453b7a
Baskin-Ridley <gbaskinridley@gmail.com>
2023-02-10 10:43:46 +0000

    diary page

--- client/index.js
+++ client/index.js
@@ -7,10 +7,6 @@ function createDiaryCard() {
     title.classList.add('diary-title');
     title.innerText = "Diary Title";
 
-    const date = document.createElement('p');
-    date.classList.add('diary-date');
-    date.innerText = "01/01/3100";
-
     const image = document.createElement('img');
     image.classList.add('diary-image');
     image.src = "https://placekitten.com/200/300";
@@ -19,10 +15,14 @@ function createDiaryCard() {
     content.classList.add('diary-content');
     content.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus ipsum sit amet elit fermentum, a aliquam arcu ornare. Pellentesque commodo massa eget leo elementum vulputate vestibulum volutpat ligula. Nullam condimentum ultrices mi et lacinia. Maecenas porta hendrerit ipsum, non sollicitudin metus feugiat vel. Quisque eu nibh vel est tincidunt pharetra. Etiam semper augue massa, vitae rhoncus velit gravida vitae. Duis rhoncus porttitor vehicula. Phasellus interdum diam ac ornare dictum. In porta dolor eros, a gravida nulla efficitur eget. Integer ullamcorper, risus id mollis gravida, felis purus suscipit massa, nec viverra libero arcu vitae nunc. Sed ut accumsan justo.";
 
+    const date = document.createElement('p');
+    date.classList.add('diary-date');
+    date.innerText = "01/01/3100";
+
     card.appendChild(title);
-    card.appendChild(date);
     card.appendChild(image);
     card.appendChild(content);
+    card.appendChild(date);
 
     document.body.appendChild(card);
 }
