3236a6562ce8d2ca4df0c21b6459a12a99077529
Baskin-Ridley <gbaskinridley@gmail.com>
2023-02-10 11:22:15 +0000

    made link work

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const diaryRouter = require("./routers/diary");
+//const diaryRouter = require("./routers/diary");
 
 const app = express();
 
